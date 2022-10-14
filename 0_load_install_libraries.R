445e7adbb0199fa17b2e33daea61c06df6d18502
gortegasolis <g.ortega.solis@gmail.com>
2022-10-14 11:47:53 +0200

    Updated workflow to query SQL on demand

--- 0_load_install_libraries.R
+++ 0_load_install_libraries.R
@@ -1,34 +1,42 @@
-#Setup the environment
-#Load packages
-pacman::p_load(tidyverse,
-               foreach,
-               taxize,
-               rscopus,
-               microdemic,
-               easyPubMed,
-               rentrez,
-               RSQLite)
+# Setup the environment
+# Load packages
+pacman::p_load(
+  tidyverse,
+  foreach,
+  taxize,
+  rscopus,
+  microdemic,
+  easyPubMed,
+  rentrez,
+  RSQLite
+)
 
-#Load api keys and other secrets
+# Load api keys and other secrets
 source("secrets.R")
 
-#Function to keep a lightweight workspace
-send2sqlite <- function(con, dataframe,tables = F){
-  RSQLite::dbWriteTable(conn = con,
-                        name = paste0("tbl_", dataframe),
-                        value = get(dataframe),
-                        overwrite = T)
-  rm(list = dataframe,
-    envir = .GlobalEnv)
-  if (tables == T){dbListTables(con)}
+# Function to keep a lightweight workspace
+send2sqlite <- function(con, dataframe, tables = F) {
+  RSQLite::dbWriteTable(
+    conn = con,
+    name = paste0("tbl_", dataframe),
+    value = get(dataframe),
+    overwrite = T
+  )
+  rm(
+    list = dataframe,
+    envir = .GlobalEnv
+  )
+  if (tables == T) {
+    dbListTables(con)
+  }
 }
 
-#Connect database
+# Connect database
 condb <- dbConnect(RSQLite::SQLite(), "LTEp.sqlite")
 dbGetInfo(condb)
-#dbDisconnect(condb)
+# dbDisconnect(condb)
 
-#readxl::excel_sheets("LTE_records_database.xlsx")
+# readxl::excel_sheets("LTE_records_database.xlsx")
 
 # for (x in readxl::excel_sheets("LTE_records_database.xlsx")){
 #   temp <- readxl::read_xlsx("LTE_records_database.xlsx",
@@ -37,4 +45,3 @@ dbGetInfo(condb)
 #   send2sqlite(condb, x)
 #   rm(temp)
 # }
-
